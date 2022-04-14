668e40ca175bb86340d6f287ac52807feca2bf95
Pierre-Yves Ricau <py@squareup.com>
2022-04-14 14:33:20 -0700

    Simplify repro

--- app/src/main/java/com/mazzouzi/rippleanimatorissue/MainActivity.kt
+++ app/src/main/java/com/mazzouzi/rippleanimatorissue/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.mazzouzi.rippleanimatorissue
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import leakcanary.AppWatcher
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, FragmentA.newInstance())
-                .commit()
-        }
+  var buttonIndex = 0
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+
+    findViewById<View>(R.id.add_button).setOnClickListener {
+      addSelfDeletingRippleButton()
     }
+  }
+
+  private fun addSelfDeletingRippleButton() {
+    buttonIndex++
+    val container = findViewById<LinearLayout>(R.id.container)
+    container.addView(Button(this).apply {
+      text = "Delete/leak me ($buttonIndex)"
+      setOnClickListener { button ->
+        container.removeView(button)
+        AppWatcher.objectWatcher.expectWeaklyReachable(button, "Button $buttonIndex removed")
+      }
+    })
+  }
 }
